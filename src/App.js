7381604afecc01c48e7e21372e565e5f5cd39e0b
Sergey Kananovich <Elfuses@gmail.com>
2024-03-27 15:58:37 +0300

    Добавление новой компоненты Dialogs
    Создание нового div блока content в котором лежат различные динамические компоненты Profile, Dialogs для разных страниц.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Header from "./Components/Header/header.js";
 import Navbar from "./Components/Navbar/navbar.js";
 import Profile from "./Components/Profile/profile.js";
+import Dialogs from "./Components/Dialogs/dialogs";
 const App = () => {
   return ( // чтобы добавить еще один блочный тег на одинаковом уровне они должны быть обернуты в общий блочный тег выше уровнем
       <div className={'app-wrapper'}>
         <Header />
         <Navbar />
-        <Profile />
+          <div className={'app-wrapper-content'}>
+              {/*<Profile />*/}
+              <Dialogs />
+          </div>
       </div>
   );
 }
--- src/Components/Profile/profile.js
+++ src/Components/Profile/profile.js
@@ -2,7 +2,7 @@ import style from "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts.js";
 const  Profile = () => {
     return (
-        <div className={style.content}>
+        <div>
             <img className={style.profile_img} alt={'profile-img'}
                  src={"https://ic.pics.livejournal.com/kapuchin/11418467/533811/533811_original.jpg"}/>
             <div className={style.item}>Some content</div>
