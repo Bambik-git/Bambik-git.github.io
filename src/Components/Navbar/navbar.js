70b1efbf8a5924cbc7c1ebb69ab003b94de35868
Sergey Kananovich <Elfuses@gmail.com>
2024-03-29 16:08:29 +0300

    Добавлены маршруты на страницы Dialogs и Profile

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
     return (
         <nav className={style.nav}>
             <ul>
-                <li><a className={style.item} href={'/#'}>Profile</a></li>
-                <li><a href={'/#'}>Messages</a></li>
+                <li><a className={style.item} href={'/profile'}>Profile</a></li>
+                <li><a href={'/dialogs'}>Messages</a></li>
                 <li><a href={'/#'}>News</a></li>
                 <li><a href={'/#'}>Music</a></li>
                 <li><a href={'/#'}>Settings</a></li>
