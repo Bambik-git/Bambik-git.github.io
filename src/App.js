2f12b1cfe277ddd775ef4bcecd15e7abfb1b0635
Sergey Kananovich <Elfuses@gmail.com>
2024-03-29 16:10:17 +0300

    Добавлены Routes
    Были добалены Routes для страниц Profile и Dialogs из react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Header from "./Components/Header/header.js";
 import Navbar from "./Components/Navbar/navbar.js";
 import Profile from "./Components/Profile/profile.js";
 import Dialogs from "./Components/Dialogs/dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 const App = () => {
   return ( // чтобы добавить еще один блочный тег на одинаковом уровне они должны быть обернуты в общий блочный тег выше уровнем
+      <BrowserRouter>
       <div className={'app-wrapper'}>
         <Header />
         <Navbar />
           <div className={'app-wrapper-content'}>
-              {/*<Profile />*/}
-              <Dialogs />
+              <Routes>
+                  <Route path="/profile" element={<Profile />}/>
+                  <Route path="/dialogs" element={<Dialogs />}/>
+              </Routes>
           </div>
       </div>
+      </BrowserRouter>
   );
 }
 
