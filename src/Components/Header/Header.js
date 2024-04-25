892dfb2c8c9067f73ff7b0d267736b8ef0b92292
Sergey Kananovich <Elfuses@gmail.com>
2024-04-25 18:44:50 +0300

    Реализована авторизация через внешний API

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import style from './Header.module.css';
+import {Link} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
+    debugger;
     return (
         <header className={style.header}>
             <img className={style.logo} src={'https://i.pinimg.com/originals/b9/05/3d/b9053d873e9f69058997913e0fffca2e.png'}
                  alt={'logo'}/>
+             <div className={style.login_block}>
+                 { props.isAuth ? props.login : <Link to={'/login'}>Login</Link>}
+             </div>
         </header>
     )
 }
--- src/Components/Header/HeaderContainer.js
+++ src/Components/Header/HeaderContainer.js
@@ -1,19 +1,41 @@
 import React from "react";
-import style from './Header.module.css';
-import {Link} from "react-router-dom";
+import Header from "./Header";
+import axios from "axios";
+import {set_user_data} from "../../redux/auth_reducer";
+import {connect} from "react-redux";
 
-const Header = () => {
+class HeaderContainer extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.instance = axios.create({
+            withCredentials: true,
+            baseURL: 'https://social-network.samuraijs.com/api/1.0',
+            headers : {
+                'API-KEY': '34f0d779-a61f-4db4-bd78-26a75ab1e0bf',
+            }
+        })
+    }
 
-    return (
-        <header className={style.header}>
-            <img className={style.logo} src={'https://i.pinimg.com/originals/b9/05/3d/b9053d873e9f69058997913e0fffca2e.png'}
-                 alt={'logo'}/>
-             <div className={style.login_block}>
-                 <Link to={'/login'}>Login</Link>
-             </div>
-        </header>
-    )
+    componentDidMount() {
+        this.instance.get(`/auth/me`).then(response => {
+            if (response.data.resultCode === 0) {
+                let {id, login, email} = response.data.data;
+                this.props.set_user_data({id, login, email});
+                console.log({id, login, email});
+            }
+        });
+
+    }
+
+    render() {
+        return <Header {...this.props}/>
+    }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login,
+});
+
+export default connect(mapStateToProps, {set_user_data})(HeaderContainer);
