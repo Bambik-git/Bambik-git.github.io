1a39a1473f9b095c6b0944f8d101b491e6a4b126
Sergey Kananovich <Elfuses@gmail.com>
2024-05-11 16:41:34 +0300

    Рефакторинг кода Добавление расширения Redux DevTool

--- src/redux/redux_store.js
+++ src/redux/redux_store.js
@@ -1,33 +1,22 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {profileReducer} from "./profile_reducer.js";
 import {dialogsReducer} from "./dialogs_reducer.js";
 import {usersReducer} from "./users_reducer";
 import {authReducer} from "./auth_reducer";
 import {thunk as thunkMiddleWare} from 'redux-thunk';
 
-let CombineReducers = combineReducers({
+let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     auth: authReducer,
 });
 
-let store = createStore(CombineReducers, applyMiddleware(thunkMiddleWare));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleWare)));
+
+// let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
 
 // window.store = store;
-
-
-const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE_NEW_MESSAGE_TEXT';
-const SEND_MESSAGE = 'SEND_MESSAGE';
-
-export const update_new_message_text_ActionCreator = (text) => {
-    return { type: UPDATE_NEW_MESSAGE_TEXT,  newMessage: text }
-}
-
-export const send_message_ActionCreator = () => {
-    debugger;
-    return { type: SEND_MESSAGE }
-}
-
 
 export default store;
