a1d9e24450dc1289be66c40df1b3227eaa6ccf96
Sergey Kananovich <Elfuses@gmail.com>
2024-05-14 12:55:33 +0300

    Добавлена ленивая загрузка компонент с помощью React.lazy и React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import './App.css';
 import Navbar from "./Components/Navbar/navbar.js";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import React from "react";
-import DialogsContainer from "./Components/Dialogs/DialogsContainer";
+import React, {lazy} from "react";
 import UsersContainer from "./Components/Users/UsersContainer.js";
-import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import Login from "./Components/Login/Login";
 
+//React.lazy
+const DialogsContainer = lazy(() => import('./Components/Dialogs/DialogsContainer'));
+const ProfileContainer = lazy(() => import('./Components/Profile/ProfileContainer'));
 
 const App = () => {
-   
-  return ( // чтобы добавить еще один блочный тег на одинаковом уровне они должны быть обернуты в общий блочный тег выше уровнем
-      <BrowserRouter>
-      <div className={'app-wrapper'}>
-        <HeaderContainer />
-        <Navbar />
-          <div className={'app-wrapper-content'}>
-              <Routes>
-                  <Route path="/profile/:userId?" element={<ProfileContainer />}/>
-                  <Route exact path="/dialogs" element={<DialogsContainer />}/>
-                  <Route exact path="/users" element={<UsersContainer />}/>
-                  <Route exact path="/login" element={<Login />}/>
-              </Routes>
-          </div>
-      </div>
-      </BrowserRouter>
-  );
+
+    return ( // чтобы добавить еще один блочный тег на одинаковом уровне они должны быть обернуты в общий блочный тег выше уровнем
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <HeaderContainer/>
+                <Navbar/>
+                <div className={'app-wrapper-content'}>
+                    <React.Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route path="/profile/:userId?" element={<ProfileContainer/>}/>
+                            <Route path="/dialogs" element={<DialogsContainer/>}/>
+                            <Route exact path="/users" element={<UsersContainer/>}/>
+                            <Route exact path="/login" element={<Login/>}/>
+                        </Routes>
+                    </React.Suspense>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
